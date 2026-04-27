eecd9db72e97bc03d54838e85383fe164006e129
renatolvicente-lang <renato_l_vicente@estudante.sesisenai.org.br>
2026-04-27 11:12:59 -0300

    add & feat - adicionei o bootstrap no codigo e adicionei o primeiro script

--- scripts/script.js
+++ scripts/script.js
@@ -2,16 +2,16 @@
 'use strict'
 
 // Fetch all the forms we want to apply custom Bootstrap validation styles to
-const forms = document.querySelectorAll('.needs-validation')
+const forms = document.querySelectorAll('#login')// seleciona todos os form com o id de login
 
 // Loop over them and prevent submission
-Array.from(forms).forEach(form => {
-form.addEventListener('submit', event => {
-if (!form.checkValidity()) {
-event.preventDefault()
-event.stopPropagation()
-}
+Array.from(forms).forEach(form => {// essa linha transforma o forms em um array e percorre ele com o forEach
+    form.addEventListener('submit', event => {// executa um evento quando ouve um submit
+        if (!form.checkValidity()) {//verifica se o formulário é valido
+            event.preventDefault()
+            event.stopPropagation()
+        }
 
-form.classList.add('was-validated')
-}, false)
+        form.classList.add('was-validated')
+    }, false)
 });
