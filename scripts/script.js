bab4ed521dce6d05e43ffcec7f10ffd04fe5ff5e
renatolvicente-lang <renato_l_vicente@estudante.sesisenai.org.br>
2026-04-27 11:35:02 -0300

    add - mas explicações

--- scripts/script.js
+++ scripts/script.js
@@ -8,11 +8,11 @@ const forms = document.querySelectorAll('#login')// seleciona todos os form com 
 Array.from(forms).forEach(form => {// essa linha transforma o forms em um array e percorre ele com o forEach
     form.addEventListener('submit', event => {// executa um evento quando ouve um submit
         if (!form.checkValidity()) {//verifica se o formulário é valido
-            event.preventDefault()
-            event.stopPropagation()
+            event.preventDefault()// não deixa enviar caso esteja vazio os inputs
+            event.stopPropagation()// não deixa um evento ser adicionado novamente
         }
-
         
+
 
 
 
